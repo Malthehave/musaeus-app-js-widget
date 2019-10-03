1d1b23b333fb6aa32a5d7f47bb5fbdb9a078f97b
Malthe Have Musaeus <malthehave@gmail.com>
2019-10-03 16:55:04 +0200

    Changed the plugin data to the a tag

--- src/widget.js
+++ src/widget.js
@@ -1,13 +1,6 @@
-var script = document.currentScript ||
-/*Polyfill*/ Array.prototype.slice.call(document.getElementsByTagName('script')).pop();
-var params = (script.getAttribute('plugin-data') || '').split(/, */);
-
-// url = "http://127.0.0.1:8000/" + params[0]; // Set the url
-url = "https://app.musaeus.dk/" + params[0]; // Set the url
-
 // Function to add a script to the dom
 function addScript(src, callback) {
-    var s = document.createElement('script'); // Create our script tag
+    const s = document.createElement('script'); // Create our script tag
     s.setAttribute('src', src); // Set the src attribute
     s.onload = callback; // And call the callback when it has been loaded
     document.body.appendChild(s); // And finaly append it to to body
@@ -15,21 +8,29 @@ function addScript(src, callback) {
 
 // The main function
 document.addEventListener('DOMContentLoaded', (event) => {
-    // DOM Should be ready
-    addScript('https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.1/iframeResizer.min.js', main);
+    // DOM should be ready
+    addScript('https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.1/iframeResizer.min.js', main); // Add the iframeresizer libary
     function main() {
-        var widget_link, iframe, i, widget_links; // Declare our variables
-        widget_links = document.getElementById('musaeus-app-widget'); // This is our widget class div, that we need to append our iframe to
-        for (i = 0; i <= Object.keys(widget_links).length; i++) { // We make a for loop if we have mutiple widgets to add an iframe to
-            widget_link = widget_links; // We make sure we're adding to the correct widget class
+        let iframe, plugin_service, styles; // Declare our variables
+        const elms = document.querySelectorAll("[id='musaeus-app-widget']"); // Get all musaeus-app-widget id's
+        for (let i = 0; i < elms.length; i++) { // Loop over each id
+            plugin = elms[i].dataset.plugin; // Get the correct plugin data
+            if (typeof(plugin) == 'undefined') { break } // If no plugin data is specified break the loop 
+            url = "http://127.0.0.1:8000/" + plugin; // Set the url for the iframe
             iframe = document.createElement('iframe'); // Let's create our iframe
             iframe.setAttribute('src', url); // The website to use in the Iframe
             iframe.setAttribute('id', 'frame' + i); // Set a unique id
             iframe.setAttribute('frameborder', '0'); // Here we make the border = 0
             iframe.setAttribute('scrolling', 'no'); // Make sure the iframe can't be scrolled
-            iframe.setAttribute('allowFullScreen', ''); // Set the allow fulscreen to true. Used for the video
-            iframe.setAttribute('style', 'min-width: 100%; min-height: 100%; border: none; margin: 0 auto; border-radius: 12px; box-shadow: 0 2px 5px rgba(0, 0, 0, .4); display: table; !important;');
-            widget_link.parentNode.replaceChild(iframe, widget_link); // And now we replace the old child with our new one (the iframe)
+            plugin_service = plugin.split('/')[0]; // Here we access the specific plugin service
+            if (plugin_service == 'video') { // We need diferent atributes for the video plugin
+                styles = 'min-width: 100%; min-height: 100%; border: none; margin: 0 auto; display: table; !important;'
+                iframe.setAttribute('allowFullScreen', ''); // Set the allow fulscreen to true. Used for the video
+            } else {
+                styles = 'min-width: 100%; min-height: 100%; border: none; margin: 0 auto; border-radius: 12px; box-shadow: 0 2px 5px rgba(0, 0, 0, .1); display: table; !important;'
+            }
+            iframe.setAttribute('style', styles); // Assign the styles that we declared eariler to the iframe tag
+            elms[i].parentNode.replaceChild(iframe, elms[i]); // And now we replace the old child with our new one (the iframe)
             iFrameResize({ log: false, bodyMargin: '10px' }, '#frame' + i); // And we use Iframe Resizer library to determine the height
         }
     }
